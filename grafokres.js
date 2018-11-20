9574c0fcbe152125d1cc6a8199fffcc912a74a1a
timichal <michal@zlatkovsky.cz>
2018-11-20 22:20:53 +0100

    modularizing

--- grafokres.js
+++ grafokres.js
@@ -1,88 +1,101 @@
-const d3 = Object.assign({}, require("d3"), require("d3-jetpack"));
-var data = [
-	{"year": 2001,    "debt": 31.4},
-	{"year": 2002,    "debt": 32.6},
-	{"year": 2003,    "debt": 34.5},
-	{"year": 2004,    "debt": 35.5},
-	{"year": 2005,    "debt": 35.6},
-	{"year": 2006,    "debt": 35.3},
-	{"year": 2007,    "debt": 35.2},
-	{"year": 2008,    "debt": 39.3},
-	{"year": 2009,    "debt": 52.3},
-	{"year": 2010,    "debt": 60.9},
-	{"year": 2011,    "debt": 65.9},
-	{"year": 2012,    "debt": 70.4},
-	{"year": 2013,    "debt": 72.6},
-	{"year": 2014,    "debt": 74.4},
-	{"year": 2015,    "debt": 73.6},
-];
+module.exports = grafokres;
+function grafokres(parameters) {
+	const d3 = require("d3");
 
-var ƒ = d3.f;
+	const elem = parameters.elem;
+	const data = parameters.data;
+	const hiddenFrom = parameters.cutoff;
+	
+	const width = Math.min(d3.select(elem).node().offsetWidth, 600);
+	const height = 400;
+	const svg = d3.select(elem)
+		.append("svg")
+		.attr("width", width)
+		.attr("height", height)
+		.attr("transform", "translate(50,50)");
 
-var sel = d3.select("body").html("");
-var c = d3.conventions({
-	parentSel: sel, 
-	totalWidth: sel.node().offsetWidth, 
-	height: 400, 
-	margin: {left: 50, right: 50, top: 30, bottom: 30}
-});
+	const xscale = d3.scaleLinear()
+		.domain([d3.min(data.map(x => x.year)), d3.max(data.map(x => x.year))])
+		.range([0, width - 100]);
 
-c.svg.append("rect").at({width: c.width, height: c.height, opacity: 0});
+	const yscale = d3.scaleLinear()
+		.domain([0, 100])
+		.range([height, 0]);
 
-c.x.domain([2001, 2015]);
-c.y.domain([0, 100]);
+	const x_axis = d3.axisBottom()
+		.scale(xscale)
+		.ticks()
+		.tickFormat(x => x);
 
-c.xAxis.ticks(4).tickFormat(ƒ());
-c.yAxis.ticks(5).tickFormat(d => d + "%");
+	const y_axis = d3.axisLeft()
+		.scale(yscale)
+		.ticks(5)
+		.tickFormat(x => x + " %");
 
-var area = d3.area().x(ƒ("year", c.x)).y0(ƒ("debt", c.y)).y1(c.height);
-var line = d3.area().x(ƒ("year", c.x)).y(ƒ("debt", c.y));
+	svg.append("g")
+		.call(y_axis);
 
-var clipRect = c.svg
-	.append("clipPath#clip")
-	.append("rect")
-	.at({width: c.x(2008) - 2, height: c.height});
+	svg.append("g")
+		.attr("transform", "translate(0, " + height  +")")
+		.call(x_axis);
 
-var correctSel = c.svg.append("g").attr("clip-path", "url(#clip)");
+	const area = d3.area().x(x => xscale(x.year)+1).y0(x => yscale(x.debt)).y1(height);
+	const line = d3.area().x(x => xscale(x.year)+1).y(x => yscale(x.debt));
 
-correctSel.append("path.area").at({d: area(data)});
-correctSel.append("path.line").at({d: line(data)});
-var yourDataSel = c.svg.append("path.your-line");
+	const clipRect = svg
+		.append("clipPath")
+		.attr("id", "clip")
+		.append("rect")
+		.attr("width", xscale(hiddenFrom) + 1)
+		.attr("height", height);
 
-d3.drawAxis(c);
+	const correctSel = svg.append("g").attr("clip-path", "url(#clip)");
 
-var yourData = data
-	.map(function(d){ return {year: d.year, debt: d.debt, defined: 0}; })
-	.filter(function(d){
-		if (d.year == 2008) d.defined = true;
-		return d.year >= 2008;
-	});
+	correctSel.append("path")
+		.attr("class", "area")
+		.attr("d", area(data));
+	correctSel.append("path")
+		.attr("class","line")
+		.attr("d", line(data));
+	const yourDataSel = svg.append("path").attr("class","your-line");
 
-var completed = false;
-
-var drag = d3.drag()
-	.on("drag", function(){
-		var pos = d3.mouse(this);
-		var year = clamp(2009, 2016, c.x.invert(pos[0]));
-		var debt = clamp(0, c.y.domain()[1], c.y.invert(pos[1]));
-
-		yourData.forEach(function(d){
-			if (Math.abs(d.year - year) < .5){
-				d.debt = debt;
-				d.defined = true;
-			}
+	// not refactored
+	var yourData = data
+		.map(d => ({year: d.year, debt: d.debt, defined: 0}) )
+		.filter(function(d){
+			if (d.year == hiddenFrom) d.defined = true;
+			return d.year >= hiddenFrom;
 		});
 
-		yourDataSel.at({d: line.defined(ƒ("defined"))(yourData)});
+	var completed = false;
 
-		if (!completed && d3.mean(yourData, ƒ("defined")) == 1){
-			completed = true;
-			clipRect.transition().duration(1000).attr("width", c.x(2015));
-		}
-	});
+	var drag = d3.drag()
+		.on("drag", function(){
 
-c.svg.call(drag);
+			if (!completed) {
+				var pos = d3.mouse(this);
+				var year = clamp(2009, 2016, xscale.invert(pos[0]));
+				var debt = clamp(0, yscale.domain()[1], yscale.invert(pos[1]));
+
+				yourData.forEach(function(d){
+					if (Math.abs(d.year - year) < .5){
+						d.debt = debt;
+						d.defined = true;
+					}
+				});
+				
+				yourDataSel.attr("d", line.defined(x => x.defined)(yourData));
+			}
+			/*
+			if (!completed && d3.mean(yourData, ƒ("defined")) == 1){
+				completed = true;
+				clipRect.transition().duration(1000).attr("width", xscale(2015));
+			}*/
+		});
+
+	svg.call(drag);
 
 
 
-function clamp(a, b, c){ return Math.max(a, Math.min(b, c)); }
+	function clamp(a, b, c){ return Math.max(a, Math.min(b, c)); }
+}
