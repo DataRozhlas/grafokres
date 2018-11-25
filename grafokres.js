dc47a6299864ca900004e24b62671dab2ca535c9
timichal <michal@zlatkovsky.cz>
2018-11-25 23:22:10 +0100

    updates

--- grafokres.js
+++ grafokres.js
@@ -6,8 +6,10 @@ function grafokres(parameters) {
 	const elem = parameters.elem;
 	const data = parameters.data;
 	const hiddenFrom = parameters.cutoff;
+	const btnFn = parameters.btnFn;
+	const interval = (parameters.interval == "decade") ? 10 : 1;
+	const yFormat = parameters.yFormat ? parameters.yFormat : "x";
 
-	
 	const width = Math.min(d3.select(elem).node().offsetWidth, 600);
 	const height = 400;
 	const svg = d3.select(elem)
@@ -15,6 +17,12 @@ function grafokres(parameters) {
 		.attr("width", width)
 		.attr("height", height)
 		.attr("transform", "translate(50,50)");
+
+	const btn = d3.select(elem)
+		.append("button")
+		.attr("class", "grafbtn disabled")
+		.attr("id",elem.substring(1)+"btn")
+		.text("Potvrdit");
 
 	const xscale = d3.scaleLinear()
 		.domain([d3.min(data.map(x => x.year)), d3.max(data.map(x => x.year))])
@@ -32,7 +40,7 @@ function grafokres(parameters) {
 	const y_axis = d3.axisLeft()
 		.scale(yscale)
 		.ticks(5)
-		.tickFormat(x => x + " %");
+		.tickFormat(x => eval(yFormat));
 
 	svg.append("g")
 		.call(y_axis);
@@ -41,17 +49,17 @@ function grafokres(parameters) {
 		.attr("transform", "translate(0, " + height  +")")
 		.call(x_axis);
 
-	const area = d3.area().x(x => xscale(x.year)+1).y0(x => yscale(x.debt)).y1(height);
-	const line = d3.area().x(x => xscale(x.year)+1).y(x => yscale(x.debt));
+	const area = d3.area().x(x => xscale(x.year)+1).y0(x => yscale(x.value)).y1(height);
+	const line = d3.area().x(x => xscale(x.year)+1).y(x => yscale(x.value));
 
 	const clipRect = svg
 		.append("clipPath")
-		.attr("id", "clip")
+		.attr("id", elem.substring(1)+"clip")
 		.append("rect")
 		.attr("width", xscale(hiddenFrom) + 1)
 		.attr("height", height);
 
-	const correctSel = svg.append("g").attr("clip-path", "url(#clip)");
+	const correctSel = svg.append("g").attr("clip-path", "url("+elem+"clip)");
 
 	correctSel.append("path")
 		.attr("class", "area")
@@ -62,7 +70,7 @@ function grafokres(parameters) {
 	const yourDataSel = svg.append("path").attr("class","your-line");
 
 	const yourData = data
-		.map(d => ({year: d.year, debt: d.debt, defined: 0}) )
+		.map(d => ({year: d.year, value: d.value, defined: 0}) )
 		.filter(function(d){
 			if (d.year == hiddenFrom) d.defined = true;
 			return d.year >= hiddenFrom;
@@ -72,15 +80,13 @@ function grafokres(parameters) {
 
 	const drag = d3.drag()
 		.on("drag", function(){
-
 			if (!completed) {
 				var pos = d3.mouse(this);
-				var year = clamp(2009, 2016, xscale.invert(pos[0]));
-				var debt = clamp(0, yscale.domain()[1], yscale.invert(pos[1]));
-
+				var year = clamp(hiddenFrom + (1 * interval), d3.max(data.map(x => x.year))+1, xscale.invert(pos[0]));
+				var value = clamp(0, yscale.domain()[1], yscale.invert(pos[1]));
 				yourData.forEach(function(d){
-					if (Math.abs(d.year - year) < .5){
-						d.debt = debt;
+					if (Math.abs(d.year/interval - year/interval) < .5){
+						d.value = value;
 						d.defined = true;
 					}
 				});
@@ -89,11 +95,21 @@ function grafokres(parameters) {
 			}
 			
 			if (!completed && (d3.mean(yourData, x => x.defined == 1) === 1)){
-				completed = true;
-				clipRect.transition().duration(1000).attr("width", xscale(d3.max(data.map(x => x.year))));
-				//console.log(yourData.map(x => Math.round(x.debt*10)/10));
+				btn.attr("class","grafbtn")	
+					.attr("data-entered", yourData.map(x => Math.round(x.value*10)/10));
 			}
 		});
+
+	btn.on("click", btnClick);
+
+	function btnClick() {
+		if (!btn.classed("disabled")) {
+			completed = true;
+			btn.style("visibility", "hidden");
+			clipRect.transition().duration(1000).attr("width", xscale(d3.max(data.map(x => x.year))));
+			btnFn(btn);
+		}
+	}
 
 	svg.call(drag);
 
