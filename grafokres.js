8838772b724ae8d5489557e366e9df93bf4acf37
timichal <michal@zlatkovsky.cz>
2019-10-24 15:34:55 +0200

    refact pt1

--- grafokres.js
+++ grafokres.js
@@ -1,151 +1,149 @@
-module.exports = grafokres;
-function grafokres(parameters) {
-	const d3 = Object.assign({}, require("d3-selection"), require("d3-axis"), require("d3-scale"), require("d3-array"),
-		require("d3-shape"), require("d3-drag"), require("d3-transition"));
+import * as d3 from "d3";
 
-	const elem = parameters.elem;
-	const data = parameters.data;
-	const hiddenFrom = parameters.cutoff;
-	const btnFn = parameters.btnFn;
-	const interval = (parameters.interval == "decade") ? 10 : 1;
-	const yFormat = parameters.yFormat ? parameters.yFormat : "x";
-	const yMin = parameters.yMin ? parameters.yMin : 0;
-	const yMax = parameters.yMax ? parameters.yMax : 100;
-	const precision = (yMax > 100) ? 1 : 10;
+export const grafokres = ({
+  elem, data, btnFn,
+  yFormat = "x", yMin = 0, yMax = 100,
+  ...parameters
+}) => {
+  const hiddenFrom = parameters.cutoff;
+  const interval = (parameters.interval === "decade") ? 10 : 1;
+  const precision = (yMax > 100) ? 1 : 10;
 
-	const width = Math.min(d3.select(elem).node().offsetWidth, 600);
-	const height = 250;
-	const svg = d3.select(elem)
-		.append("svg")
-		.attr("width", width)
-		.attr("height", height)
-		.attr("transform", "translate(50,50)");
+  const clamp = (a, b, c) => Math.max(a, Math.min(b, c));
 
-	const btn = d3.select(elem)
-		.append("button")
-		.attr("class", "grafbtn disabled")
-		.attr("id",elem.substring(1)+"btn")
-		.text("Potvrdit");
+  const width = Math.min(d3.select(elem).node().offsetWidth, 600);
+  const height = 250;
+  const svg = d3.select(elem)
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("transform", "translate(50,50)");
 
-	const xscale = d3.scaleLinear()
-		.domain([d3.min(data.map(x => x.year)), d3.max(data.map(x => x.year))])
-		.range([0, width - 100]);
+  const btn = d3.select(elem)
+    .append("button")
+    .attr("class", "grafbtn disabled")
+    .attr("id", `${elem.substring(1)}btn`)
+    .text("Potvrdit");
 
-	const yscale = d3.scaleLinear()
-		.domain([yMin, yMax])
-		.range([height, 0]);
+  const xscale = d3.scaleLinear()
+    .domain([d3.min(data.map((x) => x.year)), d3.max(data.map((x) => x.year))])
+    .range([0, width - 100]);
 
-	const x_axis = d3.axisBottom()
-		.scale(xscale)
-		.ticks((window.innerWidth < 600) ? 5 : 10)
-		.tickFormat(x => x);
+  const yscale = d3.scaleLinear()
+    .domain([yMin, yMax])
+    .range([height, 0]);
 
-	const y_axis = d3.axisLeft()
-		.scale(yscale)
-		.ticks(5)
-		.tickFormat(x => eval(yFormat));
+  const x_axis = d3.axisBottom()
+    .scale(xscale)
+    .ticks((window.innerWidth < 600) ? 5 : 10)
+    .tickFormat((x) => x);
 
-	svg.append("g")
-		.call(y_axis);
+  const y_axis = d3.axisLeft()
+    .scale(yscale)
+    .ticks(5)
+    .tickFormat((x) => eval(yFormat));
 
-	svg.append("g")
-		.attr("transform", "translate(0, " + height  +")")
-		.call(x_axis);
-	
-	const valueDisplay = svg.append("g")
-		.attr("class", "valuedisplay")
-		.append("text")
-		.attr("x", xscale(d3.max(data.map(x => x.year))));
+  svg.append("g")
+    .call(y_axis);
 
-	const introText = svg.append("g")
-		.attr("class","introtext")
-		.append("text")
-		.attr("y", height/2)
-		.attr("text-anchor","middle");
+  svg.append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(x_axis);
 
-	introText.append("tspan")
-		.attr("x", width - 100 - (width - xscale(hiddenFrom) + 1)/4)
-		.text("Sem nakreslete");
-	
-	introText.append("tspan")
-		.attr("x", width - 100 - (width - xscale(hiddenFrom) + 1)/4)
-		.attr("dy", "18")
-		.text("svůj odhad.");
-	
-	const area = d3.area().x(x => xscale(x.year)+1).y0(x => yscale(x.value)).y1(height);
-	const line = d3.area().x(x => xscale(x.year)+1).y(x => yscale(x.value));
+  const valueDisplay = svg.append("g")
+    .attr("class", "valuedisplay")
+    .append("text")
+    .attr("x", xscale(d3.max(data.map((x) => x.year))));
 
-	const clipRect = svg
-		.append("clipPath")
-		.attr("id", elem.substring(1)+"clip")
-		.append("rect")
-		.attr("width", xscale(hiddenFrom) + 1)
-		.attr("height", height);
+  const introText = svg.append("g")
+    .attr("class", "introtext")
+    .append("text")
+    .attr("y", height / 2)
+    .attr("text-anchor", "middle");
 
-	const correctSel = svg.append("g").attr("clip-path", "url("+elem+"clip)");
+  introText.append("tspan")
+    .attr("x", width - 100 - (width - xscale(hiddenFrom) + 1) / 4)
+    .text("Sem nakreslete");
 
-	correctSel.append("path")
-		.attr("class", "area")
-		.attr("d", area(data));
-	correctSel.append("path")
-		.attr("class","line")
-		.attr("d", line(data));
-	const yourDataSel = svg.append("path").attr("class","your-line");
+  introText.append("tspan")
+    .attr("x", width - 100 - (width - xscale(hiddenFrom) + 1) / 4)
+    .attr("dy", "18")
+    .text("svůj odhad.");
 
-	const yourData = data
-		.map(d => ({year: d.year, value: d.value, defined: 0}) )
-		.filter(function(d){
-			if (d.year == hiddenFrom) d.defined = true;
-			return d.year >= hiddenFrom;
-		});
+  const area = d3.area().x((x) => xscale(x.year) + 1).y0((x) => yscale(x.value)).y1(height);
+  const line = d3.area().x((x) => xscale(x.year) + 1).y((x) => yscale(x.value));
 
-	let completed = false;
+  const clipRect = svg
+    .append("clipPath")
+    .attr("id", `${elem.substring(1)}clip`)
+    .append("rect")
+    .attr("width", xscale(hiddenFrom) + 1)
+    .attr("height", height);
 
-	const drag = d3.drag()
-		.on("drag", function(){
-			introText.style("display","none");
+  const correctSel = svg.append("g").attr("clip-path", `url(${elem}clip)`);
 
-			if (!completed) {
-				const pos = d3.mouse(this);
-				const year = clamp(hiddenFrom + (1 * interval), d3.max(data.map(x => x.year))+1, xscale.invert(pos[0]));
-				const value = clamp(0, yscale.domain()[1], yscale.invert(pos[1]));
-				yourData.forEach(function(d){
-					if (Math.abs(d.year/interval - year/interval) < .5){
-						d.value = value;
-						d.defined = true;
-					}
-				});
+  correctSel.append("path")
+    .attr("class", "area")
+    .attr("d", area(data));
+  correctSel.append("path")
+    .attr("class", "line")
+    .attr("d", line(data));
+  const yourDataSel = svg.append("path").attr("class", "your-line");
 
-				const definedData = yourData.filter(x => x.defined === true);
-				const latestDefined = d3.max(definedData.map(x => x.year));
+  const yourData = data
+    .map((d) => ({ year: d.year, value: d.value, defined: 0 }))
+    .filter((d) => {
+      if (d.year === hiddenFrom) d.defined = true;
+      return d.year >= hiddenFrom;
+    });
 
-				const latestValue =  Math.round( (yourData.filter(x => x.year === latestDefined)[0].value * precision) ) / precision;
-				function latestValueFormat (x) {return eval(yFormat);}
-				
-				valueDisplay.text("● " + latestValueFormat(latestValue.toString().replace(".",",")))
-					.attr("y", yscale(latestValue) + 5);
+  let completed = false;
 
-				yourDataSel.attr("d", line.defined(x => x.defined)(yourData));
-			}
-			
-			if (!completed && (d3.mean(yourData, x => x.defined == 1) === 1)){
-				btn.attr("class","grafbtn")	
-					.attr("data-entered", yourData.map(x => Math.round(x.value*precision)/precision));
-			}
-		});
+  const drag = d3.drag()
+    .on("drag", function (){
+      introText.style("display", "none");
 
-	btn.on("click", btnClick);
+      if (!completed) {
+        const pos = d3.mouse(this);
+        const year = clamp(hiddenFrom + (1 * interval), d3.max(data.map((x) => x.year)) + 1, xscale.invert(pos[0]));
+        const value = clamp(0, yscale.domain()[1], yscale.invert(pos[1]));
+        yourData.forEach((d) => {
+          if (Math.abs(d.year / interval - year / interval) < .5){
+            d.value = value;
+            d.defined = true;
+          }
+        });
 
-	function btnClick() {
-		if (!btn.classed("disabled")) {
-			completed = true;
-			btn.style("visibility", "hidden");
-			clipRect.transition().duration(1000).attr("width", xscale(d3.max(data.map(x => x.year))));
-			btnFn(btn);
-		}
-	}
+        const definedData = yourData.filter((x) => x.defined === true);
+        const latestDefined = d3.max(definedData.map((x) => x.year));
 
-	svg.call(drag);
+        const latestValue =  Math.round( (yourData.filter((x) => x.year === latestDefined)[0].value * precision) ) / precision;
+        function latestValueFormat(x) { return eval(yFormat); }
 
-	function clamp(a, b, c){ return Math.max(a, Math.min(b, c)); }
-}
+        valueDisplay.text(`● ${latestValueFormat(latestValue.toString().replace(".", ","))}`)
+          .attr("y", yscale(latestValue) + 5);
+
+        yourDataSel.attr("d", line.defined((x) => x.defined)(yourData));
+      }
+
+      if (!completed && (d3.mean(yourData, (x) => x.defined == 1) === 1)) {
+        btn.attr("class", "grafbtn")
+          .attr("data-entered", yourData.map((x) => Math.round(x.value * precision) / precision));
+      }
+    });
+
+  btn.on("click", btnClick);
+
+  function btnClick() {
+    if (!btn.classed("disabled")) {
+      completed = true;
+      btn.style("visibility", "hidden");
+      clipRect.transition().duration(1000).attr("width", xscale(d3.max(data.map((x) => x.year))));
+      btnFn(btn);
+    }
+  }
+
+  svg.call(drag);
+};
+
+export default grafokres;
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,32 @@
 const path = require("path");
 
 module.exports = {
-	entry: "./grafokres.js",
-	mode: "production",
-	output: {
-		path: path.resolve(__dirname, "dist"),
-		filename: "grafokres.js",
-		library: "grafokres",
-		libraryTarget: "umd"
-	},
-	watch: true,
+  entry: "./grafokres.js",
+  mode: "production",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "grafokres.js",
+    library: "grafokres",
+    libraryTarget: "umd",
+  },
+  watch: true,
 
-	node: {
-		fs: "empty"
-	},
+  node: {
+    fs: "empty",
+  },
 
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /(node_modules|bower_components)/,
-				use: {
-					loader: "babel-loader",
-					options: {
-						presets: ["@babel/preset-env"/*,'@babel/preset-react'*/]
-					}
-				}
-			}
-		]
-	},
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
 };
